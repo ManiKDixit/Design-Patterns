9d8b6191458e502350dd9ba2abe8580580303f95
ManiKDixit <56781890+ManiKDixit@users.noreply.github.com>
2023-01-18 23:42:29 +0530

    Initial commit

---
+++ Singleton Pattern/SingletonDemo/SingletonPattern/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static TableServers host1LIst = TableServers.GetTableServers();
+        static TableServers host2LIst = TableServers.GetTableServers();
+
+        static void Main(string[] args)
+        {
+            TableServers servers = TableServers.GetTableServers();
+            for (int i = 0; i < 5; i++)
+            {
+                //Console.WriteLine("The next server is : " + servers.GetNextServer());
+                Host1GetNextServer();
+                Host2GetNextServer();
+            }
+
+            Console.ReadLine(); 
+
+
+
+
+        }
+
+
+
+        private static void Host1GetNextServer()
+        {
+            Console.WriteLine("The next server is : " + host1LIst.GetNextServer());
+        }
+
+
+        private static void Host2GetNextServer()
+        {
+            Console.WriteLine("The next server is : " + host2LIst.GetNextServer());
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
